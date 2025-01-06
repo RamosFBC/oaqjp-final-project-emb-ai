4e30e045bfbdb5dd118c227d03507bf63431b244
RamosFBC <felipebernardocostaramos@gmail.com>
2025-01-06 00:12:11 -0500

    Task 8

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+'''
+Module docstring
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +9,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    '''
+    method docstring
+    '''
     return render_template('index.html')
 
 @app.route('/emotionDetector')
-def emotionDetector():
+def render_detector():
+    '''
+    method docstring
+    '''
     text_to_analyse = request.args.get('textToAnalyze')
-    
     output = emotion_detector(text_to_analyse)
 
-    if output['dominant_emotion'] == None:
-        return '<strong>Invalid text! Please try again!<strong>'
+    if output['dominant_emotion'] is None:
+        return '<strong>Invalid text! Please try again!</strong>'
 
     system_response = f'''
     For the given statement, the system response is
@@ -23,9 +32,8 @@ def emotionDetector():
     'fear': {output['fear']},
     'joy': {output['joy']} and
     'sadness': {output['sadness']}.
-    The dominant emotion is <strong>{output['dominant_emotion']}<strong>.
+    The dominant emotion is <strong>{output['dominant_emotion']}</strong>.
     '''
-    
     return system_response
 
 
