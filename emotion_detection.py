9871cf017281cfd5dbcba597f56cf5f8deeaeeb7
RamosFBC <felipebernardocostaramos@gmail.com>
2025-01-05 20:30:33 -0500

    reset

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,7 +7,5 @@ def emotion_detector(text_to_analyse):
     input_json = {"raw_document": { "text": text_to_analyse } }
 
     r = requests.post(url=URL, headers=headers, json=input_json)
-    r_json = r.json()
-    text = r_json['emotionPredictions'][0]['emotionMentions'][0]['span']['text']
     
-    return text
+    return r.text
